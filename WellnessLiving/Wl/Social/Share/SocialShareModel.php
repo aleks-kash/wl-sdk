e9e103d6e9fb6595054cafa3e499a9cdfa92ac10
sdk.wellnessliving <integrations@wellnessliving.com>
2025-03-05 13:10:09 +0000

    Automatic upgrade of the SDK. Build #395108.

--- WellnessLiving/Wl/Social/Share/SocialShareModel.php
+++ WellnessLiving/Wl/Social/Share/SocialShareModel.php
@@ -7,11 +7,34 @@ use WellnessLiving\WlModelAbstract;
 
 /**
  * Saves data about share post to database.
+ * Api usage example:
+ * <code>
+ *     $o_api = new SocialShareApi();
+ *     $o_api->id_share_object = \Wl\Social\Share\ShareObjectSid::PURCHASE;
+ *     // Key of the purchase because id_share_object is \Wl\Social\Share\ShareObjectSid::PURCHASE
+ *     $o_api->a_key = [$k_purchase_a, $k_purchase_b];
+ *     $o_api->id_share_destination = \ASocialSid::FACEBOOK;
+ *     $o_api->k_business = $k_business;
+ *     $o_api->uid = $uid;
+ *     $o_api->post();
+ *
+ *     $url_link = ''; // Example: Wl_Profile_Activity_ElementModel.url_link
+ *
+ *     // s_secret points specifically to your object that you want to share, so add it to the url.
+ *     // In this case url_link stores all purchases for the business,
+ *     // and by using s_secret we will get a link to only those purchases we want to share.
+ *     $url_facebook_share = 'https://www.facebook.com/sharer/sharer.php?u=' . urlencode($url_link . '&s_secret=' . $o_api->s_secret);
+ * </code>
  */
 class SocialShareModel extends WlModelAbstract
 {
   /**
-   * The primary keys of the shared objects.
+   * The primary keys of the shared objects. Depends on {@link SocialShareModel::$id_share_object}.
+   * If {@link SocialShareModel::$id_share_object} is:
+   * * {@link ShareObjectSid::BOOK} - each value is key of the visit. Primary key in {@link \RsVisitSql} table.
+   * * {@link ShareObjectSid::LOCATION} - each value is key of the location. Primary key in {@link \RsLocationSql} table.
+   * * {@link ShareObjectSid::PURCHASE} - each value is key of the purchase. Primary key in {@link \RsPurchaseSql} table.
+   * * {@link ShareObjectSid::REVIEW} - each value is key of the review. Primary key in {@link \RsReviewSql} table.
    *
    * @post post
    * @var string[]
@@ -43,7 +66,7 @@ class SocialShareModel extends WlModelAbstract
   public $k_business;
 
   /**
-   * Secret code shared object.
+   * Secret key for access shared object.
    *
    * @post result
    * @var string
--- WellnessLiving/WlModelRequest.php
+++ WellnessLiving/WlModelRequest.php
@@ -12,7 +12,7 @@ class WlModelRequest
   /**
    * SDK version number.
    */
-  const VERSION='20250304.394752';
+  const VERSION='20250305.395108';
 
   /**
    * A list of headers for the API request. See {@link CURLOPT_HTTPHEADER}.
